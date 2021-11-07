18de5534e6f86d6c62a32638bd17b78cba2e2edf
Dainsleif Fractal <0x80020006@gmail.com>
2021-11-07 22:55:59 +0900

    スクロールによるPictureBoxを動的に追加

--- MainForm.cs
+++ MainForm.cs
@@ -19,6 +19,7 @@ namespace Test_Program3
         List<string> filesList;
         PictureBox[] pbList;
         bool LoadFlag;
+        int msFlag;
 
         static readonly int MS_WIDTH  = 500;
         static readonly int MS_HEIGHT = 470;
@@ -28,8 +29,7 @@ namespace Test_Program3
         static readonly int CM_HEIGHT = 1080;
         static readonly int IMG_BETWEEN = 10;
         static readonly int BAR_HEIGHT = SystemInformation.CaptionHeight + SystemInformation.MenuHeight;
-        static readonly int FILE_RANGE = 2
-            ;
+        static readonly int FILE_RANGE = 2;
 
         public MainForm()
         {
@@ -114,17 +114,22 @@ namespace Test_Program3
                 if (e.Delta > 0 && fNumber > 0)
                 {
                     fNumber -= 1;
+                    msFlag = -1;
                     FileRange_Math();
                 }
                 else if (e.Delta < 0 && fNumber < filesList.Count - 1)
                 {
                     fNumber += 1;
+                    msFlag = 1;
                     FileRange_Math();
                 }
 
-                if (this.Contains(pbList[fNumber]) == false)
+                if (fNumber + (FILE_RANGE * msFlag) >= 0 && fNumber + (FILE_RANGE * msFlag) < filesList.Count)
                 {
-                    Img_Load();
+                    if (this.Contains(pbList[fNumber + (FILE_RANGE * msFlag)]) == false)
+                    {
+                        Img_Add();
+                    }
                 }
                 PBList_Location();
 
@@ -174,18 +179,17 @@ namespace Test_Program3
 
         void Img_Load()
         {
-            for (int i = fNumberM; i < fNumberP; i++)
+            for (int i = fNumberM; i <= fNumberP; i++)
             {
                 if (i >= 0 && i < filesList.Count)
                 {
 
-                    //if (this.Contains(pbList[i]) == false)
-                    //{
                     if (this.Contains(pbList[i]) == true)
                     {
                         pbList[i].Image.Dispose();
                         pbList[i].Image = null;
                         Controls.Remove(pbList[i]);
+                        Console.WriteLine($"pbList[{i}] removed!");
                     }
                     fPath = filesList[i];
                         Console.WriteLine($"{fPath}");
@@ -226,8 +230,55 @@ namespace Test_Program3
 
                         pbList[i].Image = b;
                     Invalidate();
-                    //}
                 }
+            }
+        }
+
+        void Img_Add()
+        {
+            int i = fNumber + (FILE_RANGE * msFlag);
+            if (i >= 0 && i < filesList.Count)
+            {
+                fPath = filesList[i];
+                Console.WriteLine($"{fPath}");
+
+                pbList[i] = new PictureBox();
+                pbList[i].BackColor = Color.Blue;
+                Controls.Add(pbList[i]);
+                //Image img = Image.FromFile(fPath);
+                FileStream fs = File.OpenRead(fPath);
+                Image img = Image.FromStream(fs, false, false);
+
+                float iW = img.Width;
+                float iH = img.Height;
+                Console.WriteLine($"{iW},{iH}");
+                float cW = Width;
+                float cH = Height - BAR_HEIGHT;
+                Console.WriteLine($"{cW},{cH}");
+
+
+                float rW = cW / iW;
+                float rH = cH / iH;
+                float r = Math.Min(rW, rH);
+                Console.WriteLine($"{r}");
+                int mW = (int)(img.Width * r);
+                int mH = (int)(img.Height * r);
+
+                pbList[i].Width = mW;
+                pbList[i].Height = mH;
+                pbList[i].Location = new Point(ClientSize.Width / 2 - pbList[i].Width / 2 + (pbList[i].Width + IMG_BETWEEN) * (i - fNumber), SystemInformation.MenuHeight + (ClientSize.Height - SystemInformation.MenuHeight) / 2 - pbList[i].Height / 2);
+
+                Bitmap b = new Bitmap(mW, mH);
+                Graphics g = Graphics.FromImage(b);
+
+                g.DrawImage(img, 0, 0, mW, mH);
+
+                img.Dispose();
+                g.Dispose();
+
+                pbList[i].Image = b;
+                Invalidate();
+
             }
         }
     }
